1cc519ecbf75ccc065db986c19d58f448d4838a1
kk-1590 <kkhandelwal1590@gmail.com>
2024-08-10 07:45:33 +0530

    structural directives

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+    products = [
+      { id: 1, name: 'Product 1', price: 100,color:'Black',status:"Available"},
+      { id: 2, name: 'Product 2', price: 300,color:'Blue',status:"Not Available"},
+      { id: 3, name: 'Product 3', price: 400,color:'Red',status : "Available"},
+      { id: 4, name: 'Product 4', price: 300,color:'Salmon',status : "Available"}
+    ]
 }
